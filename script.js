62fa6d08ea36fda180e549eafd6f376300a7d327
RodrigoLuigi <digaor@gmail.com>
2022-07-27 21:00:50 -0300

    Atualizando

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+const form = document.querySelector('form');
+const modal = document.getElementById('modal');
+const btnClosed = document.getElementById('btnClosed');
+
+let timeout;
+
+
 function modalClose(){
-  document.getElementById('modal').style.display = 'none';
+  btnClosed.addEventListener("click", event =>{
+    event.preventDefault();
+    form.classList.add("form-topout");
+    timeout = setTimeout(closed, 400)
+  
+  });
 }
+
+function closeAnimate(){
+  form.classList.add("form-topout");
+}
+
+function closed(){
+  modal.style.display = 'none';
+}
+
 
 function openForm(){
   document.getElementById('modal').style.display = 'block'
